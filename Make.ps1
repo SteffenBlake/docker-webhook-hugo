a9f5b94c7328861e0341adcf0d0b9642abc68d63
Steffen Blake <steffen.cole.blake@gmail.com>
2021-09-15 00:21:11 -0600

    Create Make.ps1

---
+++ Make.ps1
@@ -0,0 +1,28 @@
+$REPO = "steffenblake/webhook-hugo"
+$VER = "1.0.0"
+
+$OSLIST = "linux"
+$ARCHLIST = @{
+    # arm = "arm/v7"; TODO: Add ARMv7 support (Need special logic in docker file for moosefs apt-get to use raspbian image)
+    arm64 = "arm64"; 
+    amd64 = "amd64" 
+}
+
+$ManifestLatest = $Repo + ":latest"
+$ManifestVer = $Repo + ":" + $VER
+
+ForEach ($ARCH in $ARCHLIST.keys) {
+  $ArchRepo = $Repo + ":linux-" + $ARCH
+  $LatestRepo = $ArchRepo + "-latest"
+  $VerRepo = $ArchRepo + "-" + $VER
+  Write-Output "Pushing to $LatestRepo / $VerRepo"
+  docker buildx build . --platform linux/$ARCH -t $LatestRepo -t $VerRepo --push
+  Write-Output "Composing to Manifests $ManifestLatest / $ManifestVer"
+  docker manifest create $ManifestLatest --amend $LatestRepo
+  docker manifest create $ManifestVer --amend $VerRepo
+}
+
+Write-Output "Publishing to Manifests $ManifestLatest / $ManifestVer"
+
+docker manifest push $ManifestLatest
+docker manifest push $ManifestVer
